fa5a0c54465e5985a34fe31be538efeeec246690
Graham Newman <graham@fracturedgraphics.com>
2017-01-15 08:26:50 +0000

    Delete config.js

--- node22/config.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  consumer_key:         'rng46Xl5AVBikSbGFq2tS9jd7',
-  consumer_secret:      'QOqEjWsErHqWBKIBGCrMyPfLknmW3tPCNmkmdwHUUxvjgpfYzN',
-  access_token:         '775784990063951872-P4Um0FpV9iAv1zn0HvC8amNoAsWjPlk',
-  access_token_secret:  'Br0khrxOp4LFpE2SCnnWvR6qA5O2oPIBmOUa4ZUY2n5Ow',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
